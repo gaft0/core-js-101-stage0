6fc2c87c26eaf8e5157703698ff1012d924aa9c9
Александр Плесовских <sasha301106@gmail.com>
2025-08-09 23:30:03 +0300

    feat: make numbers tasks #2

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -85,8 +85,8 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return -b / a;
 }
 
 /**
@@ -123,8 +123,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
@@ -155,8 +155,9 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  const d = a ** 2 + b ** 2 + c ** 2;
+  return Math.sqrt(d);
 }
 
 /**
@@ -176,8 +177,29 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  switch (pow) {
+    case 0:
+      return num;
+    case 1:
+      if (num % 10 >= 5) {
+        return num - (num % 10) + 10;
+      }
+      return num - (num % 10);
+    case 2:
+      if (num % 100 >= 50) {
+        return num - (num % 100) + 100;
+      }
+      return num - (num % 100);
+    case 3:
+      if (num % 1000 >= 500) {
+        return num - (num % 1000) + 1000;
+      }
+      return num - (num % 1000);
+    default:
+      break;
+  }
+  return 0;
 }
 
 /**
